df3f7bbf6c9fda9772a8646050c2b3aa36d2cc18
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-10 11:52:51 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+function click1(){
+    document.querySelector( "#para" ).innerHTML ="you have clicked button1"
+}
+
+function click2(){
+    document.querySelector( "#para" ).innerHTML = "you have clicked button2"
+}
+
+
+function click3(){
+    document.querySelector( ".para1" ).innerHTML = "there are lots of information if you would like to get more information so you can do right now"
+}
+function click4(){
+    document.querySelector( ".para1" ).innerHTML = "there are lots of information if you would like to get more information so you can do right now"
+}
